64b225c1182b1a49d0ff0eae7d0dddd096552b2d
ne-peer <mileage0110@gmail.com>
2018-08-09 12:25:12 +0900

    update: headless browser open to just once

--- cpsearch.js
+++ cpsearch.js
@@ -12,17 +12,21 @@ const SEARCH_URL_NOVEL = "https://www.pixiv.net/novel/search.php?word=";
  *
  * @param {string} keyword 検索ワード
  */
-const crawler = async (keyword, searchHost) => {
-  // "--no-sandbox"で実行: https://github.com/GoogleChrome/puppeteer/issues/290
-  const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
-  const page = await browser.newPage();
-
-  // 検索実行
-  await page.goto(searchHost + keyword);
-  const html = await page.$eval("html", elem => elem.innerHTML);
-  await browser.close();
-
-  return html;
+const crawler = {
+  open: async () => {
+    // "--no-sandbox"で実行: https://github.com/GoogleChrome/puppeteer/issues/290
+    const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+    this.browser = await browser.newPage();
+    await console.log("Process: Browser openned.");
+  },
+  close: async () => {
+    await this.browser.close();
+    await console.log("Process: Browser closed.");
+  },
+  search: async (keyword, searchHost) => {
+    await this.browser.goto(searchHost + keyword);
+    return await this.browser.$eval("html", elem => elem.innerHTML);
+  }
 };
 
 /**
@@ -46,11 +50,17 @@ const search = config => {
     const host = config.type_novel === true ? SEARCH_URL_NOVEL : SEARCH_URL_IMAGE;
     const type = config.type_novel === true ? "Novels" : "Pictures";
 
+    // starting headless browser
+    await crawler.open();
+
     for (const word of words) {
-      const html = await crawler(word, host);
+      const html = await crawler.search(word, host);
       const count = await scrapingResultCount(html);
-      await console.log(`${type} count of "${word}" : ${count}`);
+      await console.log(`${type} count of "${word}" = ${count}`);
     }
+
+    // browser end
+    await crawler.close();
 
     console.log("terminated.");
   })().catch(err => {
@@ -64,7 +74,8 @@ const search = config => {
 cli.command(
   "run",
   {
-    desc: "Get CP seach result count on pixiv. Require search words setting of config.json file."
+    desc:
+      "Get CP seach result count on pixiv. Require search words setting of config.json file."
   },
   () => {
     search(config);
